d5091680979c4ce0bdc5bb602743bfe21d00668a
ssevtaparslan <sevtaparslan37@gmail.com>
2025-03-21 03:11:50 +0300

    Add files via upload

---
+++ MerhabaDunya/MerhabaDunya/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Basit Hesap Makinesi");
+        Console.WriteLine("İşlemler: +, -, *, /");
+
+        try
+        {
+            Console.Write("Birinci sayıyı girin: ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı girin: ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İşlem seçin (+, -, *, /): ");
+            char islem = Convert.ToChar(Console.ReadLine());
+
+            switch (islem)
+            {
+                case '+':
+                    Console.WriteLine($"Sonuç: {sayi1} + {sayi2} = {sayi1 + sayi2}");
+                    break;
+                case '-':
+                    Console.WriteLine($"Sonuç: {sayi1} - {sayi2} = {sayi1 - sayi2}");
+                    break;
+                case '*':
+                    Console.WriteLine($"Sonuç: {sayi1} * {sayi2} = {sayi1 * sayi2}");
+                    break;
+                case '/':
+                    if (sayi2 != 0)
+                        Console.WriteLine($"Sonuç: {sayi1} / {sayi2} = {(double)sayi1 / sayi2}");
+                    else
+                        Console.WriteLine("Hata: Sıfıra bölme yapılamaz!");
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz işlem girdiniz!");
+                    break;
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Lütfen geçerli bir tam sayı girin!");
+        }
+    }
+}
